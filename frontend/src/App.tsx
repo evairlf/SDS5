f4938160bfc1f23b0fec869a55ecbb392e17e941
Evair Leite Feldmann <evairleitefeldmann@gmail.com>
2021-11-01 10:40:57 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/data_table";
+import DonutChart from "components/DonutChart";
 import Footer from "components/footer";
 import NavBar from "components/navBar";
 
@@ -8,8 +10,25 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Hello World!</h1>
+        <h1 className="text-primary py-3">Hello World!</h1>
 
+        <div className="row px-3">
+        <div className="col-sm-6">
+
+          <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+          <BarChart />
+        </div>
+        <div className="col-sm-6">
+
+          <h5 className="text-center text-secondary">Todas Vendas</h5>
+          <DonutChart />
+        </div>
+
+        </div>
+
+      </div>
+      <div className="py-3">
+      <h2 className="text-primary">Todas As Vendas</h2>
       </div>
       <DataTable />
       <Footer />
