e578097a00a428f2bcb4bef6ccf01b6c653ab93d
Evair Leite Feldmann <evairleitefeldmann@gmail.com>
2021-11-07 11:40:00 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,38 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/data_table";
+import DonutChart from "components/DonutChart";
+import Footer from "components/footer";
+import NavBar from "components/navBar";
+
+const Dashboard = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de vendas</h1>
+
+                <div className="row px-3">
+                    <div className="col-sm-6">
+
+                        <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+
+                        <h5 className="text-center text-secondary">Todas Vendas</h5>
+                        <DonutChart />
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="py-3">
+                <h2 className="text-primary">Todas As Vendas</h2>
+            </div>
+            <DataTable />
+            <Footer />
+        </>
+    );
+}
+
+export default Dashboard;
